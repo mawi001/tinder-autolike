27fabd6847f203258758c18b324642abba9fd3c4
mawi001 <mwillemsma@gmail.com>
2020-09-15 22:41:26 +0200

    he made me do it

--- autolike.js
+++ autolike.js
@@ -10,6 +10,8 @@ function hasBlacklistKeywords(bio) {
 			'trans',
 			'shemale',
 			'chubby',
+			'trans',
+			'disney boy',			
 			//' lb ',
 		];
 
@@ -51,7 +53,7 @@ function trickTinder() {
 
 	var infoClassName = 'recCard__info';
 	var dislike = document.getElementsByClassName("button")[0];
-	var like = document.getElementsByClassName("button")[2];
+	var like = document.getElementsByClassName("button")[3];
 
 	// Open profile bio
 	var info = document.getElementsByClassName(infoClassName)[0];
